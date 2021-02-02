ba35f846fa9d8bf714c64c70f2c361330bcd32d9
mjbeauchamp <monica.j.beauchamp@gmail.com>
2021-02-02 14:27:28 -0700

    Adding image assets

---
+++ ng-fundamentals/src/app/events-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: `
+  <h1>Hellooo!!</h1>
+  <img src="/assets/images/basic-shield.png" />
+  `
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals';
+}
